ce22adc54677fefad33b2b748f9fdab8b560566a
Vinicius Fontes <vinif.santos2002@gmail.com>
2023-12-12 15:16:54 -0300

    processos relacionados aos graficos foram comentados

--- main.js
+++ main.js
@@ -172,5 +172,5 @@ for (const formElement of form) {
   }
 }
 
-form.addEventListener("submit", renderProgression);
+// form.addEventListener("submit", renderProgression);
 clearFormButton.addEventListener("click", clearForm);
